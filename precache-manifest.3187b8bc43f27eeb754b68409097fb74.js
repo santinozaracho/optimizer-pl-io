5a58233c72f02073e30de05c216037e927b72559
Carlos Santino Zaracho Simonetto <40505201+santinozaracho@users.noreply.github.com>
2019-05-19 14:34:33 -0300

    Updates

---
+++ precache-manifest.3187b8bc43f27eeb754b68409097fb74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41990cd8d17a92b489964ebd08a5923f",
+    "url": "/optimizer-pl-io/index.html"
+  },
+  {
+    "revision": "af13299b6f880dd88db3",
+    "url": "/optimizer-pl-io/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/optimizer-pl-io/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "af13299b6f880dd88db3",
+    "url": "/optimizer-pl-io/static/js/main.2f061edc.chunk.js"
+  },
+  {
+    "revision": "94bc843c89792bd532e7",
+    "url": "/optimizer-pl-io/static/js/runtime~main.718f7bf5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/optimizer-pl-io/static/media/logo.5d5d9eef.svg"
+  }
+]);
