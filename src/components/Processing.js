6cd9659d9334b9ed959c870d88d074b39c1cf7a6
Carlos Santino Zaracho Simonetto <carlosszaracho@gmail.com>
2019-06-06 10:31:02 -0300

    Correccion del manejo de info de variables y restricciones

--- src/components/Processing.js
+++ src/components/Processing.js
@@ -9,32 +9,38 @@ class Processing extends React.Component{
     constructor (props){
         super(props);
         this.state={};
-        this.handleCoeficientefromV=this.handleCoeficientefromV.bind(this);
-        this.handlerInputRes=this.handlerInputRes.bind(this);
-        this.handlerInputVar=this.handlerInputVar.bind(this);
-
+        this.handleCoefVar=this.handleCoefVar.bind(this);
+        this.handleCoefRes=this.handleCoefRes.bind(this);
     }
 
-
-    handlerInputVar (event) {
-        let {value, name} = event.target;   
-    }
-
-    handlerInputRes (event) {
-        let {value, name} = event.target;
-        let {restricciones} = this.props.status;
-        restricciones[name] = value;
-        this.props.handleRestricciones(restricciones);
-    }
     //En el Siguiente Handler, Se toma del input de una variable en particular el coeficiente.
-    handleCoeficientefromV(indVariable,coeficiente) {
-        
+    handleCoefVar(event) {
+        let {value, name} = event.target;
         let {variables} = this.props.status;
-        variables[indVariable].coeficiente = coeficiente;
-        console.log(variables);
-        
+        variables[name].coeficiente = parseInt(value);
         this.props.handleVariables(variables);
     }
+
+    handleCoefRes(event,ri) {
+        let {name,value } = event.target
+        let {restricciones} = this.props.status;
+        console.log('En la Res:'+ri+', en el campo:'+name+',con el valor:'+value);
+        
+        switch (name) {
+            case 'derecha':
+                    restricciones[ri].derecha = parseInt(value)
+                break;
+            case 'eq':
+                    restricciones[ri].eq = parseInt(value)
+                break;
+            default:
+                    restricciones[ri].coeficientes[name]=parseInt(value)
+                break;
+        }
+        console.log(restricciones);
+        this.props.handleRestricciones(restricciones);
+    }
+
 
     render() {
         //Obtenemos las propiedades del Super
@@ -43,12 +49,16 @@ class Processing extends React.Component{
         console.log(restricciones);
 
         //Generamos el renderizado para cada una de los elementos de los arreglos obtenidos anteriormente.
-        let variablesToDesc = variables.map( (variable,index) => {       
-            return(<Variable key={'V'+index} getCoeficiente={this.handleCoeficientefromV} variable={variable}/>)
+        let variablesToDesc = variables.map( (variable,index) => {
+            if (variable.descripcion !== '') {
+                return(<Variable key={'V'+index} handleCoefVar={this.handleCoefVar} variable={variable}/>)
+            }       
         });
 
         let restriccionesInput = restricciones.map((restriccion,index) => {
-            return(<Restriccion key={'R'+index} restriccion={restriccion}/>)
+            if (restriccion.descripcion !== '') {
+                return(<Restriccion key={'R'+index} handleCoefRes={this.handleCoefRes} restriccion={restriccion}/>)
+            }
         });
         
         return(
