d002a48d3b3d6c511efb76739c94ea35bd525609
Carlos Santino Zaracho Simonetto <carlosszaracho@gmail.com>
2019-06-06 10:29:17 -0300

    Incorporacion del Modulo Solver Simplex

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import {Alert} from 'reactstrap';
+import {Alert,Button} from 'reactstrap';
+import solver from 'javascript-lp-solver'
+import Restriccion from './elements/Restriccion';
+
+let convertArraysToModelForSolver = (modelo) => {
+    let {restricciones,variables,objective} = modelo;
+    let model = {};
+    model.constraints = restricciones;
+    
+    return JSON.stringify(model)
+}
+        
+
 
 class Presentation extends React.Component{
     constructor (props){
         super(props)
-        this.state={
-            params:""
-        }
-
+        this.state={model:''}
     }
+
+    calculate = () => {
+        let model = convertArraysToModelForSolver(this.props.status)
+        console.log(model);
+        
+        
+    }
+
+
     render () {
+        
+
         return(
-            <Alert color="warning">HELLO PRESENETATIOANON</Alert>
+            <> 
+            <Alert color="warning">TESTING RESULTS</Alert>
+            <Button color='success' onClick={this.calculate}>Calcular</Button>
+            <p>{this.state.resultados}</p>
+            </>
 
         )
     }
