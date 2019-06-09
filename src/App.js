2fda85e931605170b18560e02ee130ab4af96d91
Carlos Santino Zaracho Simonetto <carlosszaracho@gmail.com>
2019-06-09 14:08:24 -0300

    Se incorporo una pagina de inicio, e instalo react dom para manejar los tipos de PL

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { Button,Jumbotron,Container,Row,Col } from "reactstrap";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import  LinealProg  from "./components/LinealProg";
 
-function App() {
+
+const LinealProgramacion = () => <LinealProg></LinealProg>;
+
+const NoLinealProgramacion = () =>  <Container fluid className="App">
+                                      <Row className="">
+                                          <Col xs={12} md={6}  className="mx-auto my-5">
+                                            <Jumbotron className=''>
+                                              <Row><h2>La PL No-Lineal recien estara disponible en el 2020.</h2></Row>
+                                              <Row><Link to={'/'}><Button className='fluid-left'>Volver</Button></Link></Row>
+                                            </Jumbotron>       
+                                          </Col>                    
+                                      </Row>
+                                    </Container>;
+
+const Index = () => 
+                  <Container fluid className="App">
+                    <Row className="">
+                        <Col xs={12} md={6}  className="mx-auto my-5">
+                          <Jumbotron>
+                            <Row><h2 className='mx-auto'>Bienvenido!!!! Seleccione una opcion.</h2></Row>
+                            <Row>
+                              <Col><Link to={'/linealProg'}><Button color='success'>Programacion Lineal </Button></Link></Col>
+                              <Col><Link to={'/noLinealProg'}><Button disabled color='success'>Programacion No Lineal </Button></Link></Col>
+                            </Row>
+                            <Row>
+                              <h5>Desarrolladores:</h5>
+                            </Row>
+                          </Jumbotron>       
+                        </Col>                    
+                    </Row>
+                  </Container>;
+
+const App = () => {
   return (
-    <LinealProg></LinealProg>
+    <Router>
+        <Route exact path="/" component={Index} />
+        <Route path="/linealProg" component={LinealProgramacion} />
+        <Route path="/noLinealProg/" component={NoLinealProgramacion} />
+        
+    </Router>
   );
 }
 
