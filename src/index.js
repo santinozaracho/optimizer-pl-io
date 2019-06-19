c6cbef1c20d0315ad1b9fd02d7867bde62e08bee
Carlos Santino Zaracho Simonetto <40505201+santinozaracho@users.noreply.github.com>
2019-06-19 19:56:29 -0300

    import del css para visaulizar correctamente el grafico

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import './index.css';
+import 'react-vis/dist/style.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
