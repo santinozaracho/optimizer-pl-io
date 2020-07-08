1eabcb3558349ce78014498a667afe15a70fc22d
Nicolas <nikoalegre@hotmail.com>
2020-07-08 18:23:15 -0300

    Agregue los calculos basicos de modelo de stock simple, el front aun no esta completo

--- src/components/StockProblems/ModelStockSimple/index.js
+++ src/components/StockProblems/ModelStockSimple/index.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { ButtonGroup, Button, Container, Row, Col, Card, CardBody, CardHeader, CardTitle, Jumbotron } from "reactstrap";
-import {InputGroupText,InputGroup, Input,InputGroupAddon,PopoverBody} from 'reactstrap';
-import { Alert, UncontrolledPopover, PopoverHeader } from "reactstrap";
-import ReactWizard from "react-bootstrap-wizard";
+import { ButtonGroup, Button, Container, Row, Col, Card, CardBody, CardHeader, CardTitle, Jumbotron} from "reactstrap";
+import {Link} from 'react-router-dom';
 
 
 class modelStockSimple extends React.Component{
@@ -19,11 +17,32 @@ class modelStockSimple extends React.Component{
         this.setState({
             [event.target.name]: event.target.value
         })
+        this.calcularCosto();
+    }
+    
+    calcularCosto(){
+        let {demanda, costoDePreparacion, costoDeAlmacenamiento} = this.state;
+        return Number(costoDePreparacion) + Number(costoDeAlmacenamiento);
     }
 
+    calcularInventarioOptimo(){
+        let {demanda, costoDePreparacion, costoDeAlmacenamiento} = this.state;
+        return (Math.sqrt((2*Number(costoDePreparacion)*Number(demanda))/(Number(costoDeAlmacenamiento)))); 
+    }
+
+    calcularLongitud(){
+        let {demanda, costoDePreparacion, costoDeAlmacenamiento} = this.state;
+        let inventario = this.calcularInventarioOptimo();
+        return (inventario/Number(demanda)); 
+    }
     
+
     render() { 
-        let {demanda} = this.state;
+        let {demanda, costoDePreparacion, costoDeAlmacenamiento} = this.state;
+        let costo = this.calcularCosto();
+        let inventario = this.calcularInventarioOptimo();
+        let longitud = this.calcularLongitud();
+        
         return (
             <Container fluid className="App">
             <Row>
@@ -37,14 +56,27 @@ class modelStockSimple extends React.Component{
                         <div>
                             <form>
                                 <p><input type="text" placeholder="Ingresar demanda" name="demanda" onChange={this.handleInputChange}></input></p>
+                                <p><input type="text" placeholder="Ingresar costo de preparacion" name="costoDePreparacion" onChange={this.handleInputChange}></input></p>
+                                <p><input type="text" placeholder="Ingresar costo de almacenamiento" name="costoDeAlmacenamiento" onChange={this.handleInputChange}></input></p>
                             </form>
                         </div>              
                     </Col>
 
                     <Col>
-                        <h3>Tu demanda es: {demanda}</h3>
+                        <h6>Tu demanda es: {demanda}</h6>
+                        <h6>Tu costo de preparacion es: ${costoDePreparacion}</h6>
+                        <h6>Tu costo de almacenamiento es: ${costoDeAlmacenamiento}</h6>
+                        <h4>El costo total es de: ${costo}</h4>
+                        <h4>Cantidad economica de pedido y*= {inventario}</h4>
+                        <h4>Longitud del ciclo t0*= {longitud}</h4>
                     </Col>
 
+                    <Row className="btn-volver justify-content-center">
+                        <Button href="#" color="primary">Calcular</Button>
+                    </Row>
+                    <Row>
+                        <Link to='./'><Button>Volver</Button></Link>
+                    </Row>
                 </Jumbotron>
               </Col>
             </Row>
