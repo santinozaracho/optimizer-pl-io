50e6c84f4ce8a754d110afc9968a0241ae51e340
Nicolas <nikoalegre@hotmail.com>
2020-07-08 23:47:22 -0300

    Funciona todo del modelo ModelStockSimple, faltaria agregar algunos controles de input y mejorar el front de la respuesta

--- src/components/StockProblems/ModelStockSimple/index.js
+++ src/components/StockProblems/ModelStockSimple/index.js
@@ -23,16 +23,17 @@ class modelStockSimple extends React.Component{
         })
     }
     
-    calcularCostoInventario()
-    {
-        let {demanda, costoDePreparacion, costoDeAlmacenamiento} = this.state;
-        let promedioInventario = this.calcularInventarioOptimo / 2;
-        return costoDePreparacion/(this.calcularInventarioOptimo/demanda)+costoDeAlmacenamiento*promedioInventario; //TCL(y)
-    }
-
     calcularInventarioOptimo(){
         let {demanda, costoDePreparacion, costoDeAlmacenamiento} = this.state;
         return (Math.sqrt((2*Number(costoDePreparacion)*Number(demanda))/(Number(costoDeAlmacenamiento)))); //y*
+    }
+
+    calcularCostoInventario()
+    {
+        let {demanda, costoDePreparacion, costoDeAlmacenamiento} = this.state;
+        let y = this.calcularInventarioOptimo();
+        let promedioInventario = (y / 2);
+        return ((costoDePreparacion/(y /demanda))+ (costoDeAlmacenamiento*promedioInventario)); //TCL(y)
     }
 
     calcularLongitud(){
@@ -44,15 +45,15 @@ class modelStockSimple extends React.Component{
     calcularPuntoDeReorden(){
         let {demanda,politica,tiempoDeEntrega} = this.state;
         let duracionCicloDePedido = this.calcularLongitud();//to*
-        if(tiempoDeEntrega > duracionCicloDePedido)
-        {//para politica 1
-            politica=true;
-            let numeroEntero = Math.trunc(tiempoDeEntrega/duracionCicloDePedido);//n
-            let tiempoEfectivoDeEntrega= tiempoDeEntrega- numeroEntero* duracionCicloDePedido;//Lc
-            //return puntoDeReorden= tiempoEfectivoDeEntrega * demanda;//punto de reorden
+        if(tiempoDeEntrega > duracionCicloDePedido){ //SI L > to*, calculamos Le
+        //para politica 1
+            
+            let n = Math.trunc(tiempoDeEntrega/duracionCicloDePedido);//n
+            let tiempoEfectivoDeEntrega= tiempoDeEntrega - (n * duracionCicloDePedido);//Le
+            return (tiempoEfectivoDeEntrega * demanda);//punto de reorden
         }else{
             //para politica 2
-            return tiempoDeEntrega;
+            return duracionCicloDePedido;
         }
     }
 
@@ -67,10 +68,18 @@ class modelStockSimple extends React.Component{
     //sino
     //Pedir la cantidad {CalcularInventarioOptimo()} siempre que la cantidad de inventario baje de {CalcularPuntoDeReorden()} unidades
     render() { 
-        let {demanda, costoDePreparacion, costoDeAlmacenamiento} = this.state;
+        let {demanda, costoDePreparacion, costoDeAlmacenamiento, tiempoDeEntrega} = this.state;
         //let costo = this.calcularCosto();
         let inventario = this.calcularInventarioOptimo();
         let longitud = this.calcularLongitud();
+        let TCU = this.calcularCostoInventario();
+        let puntoDeReorden = this.calcularPuntoDeReorden();
+
+        //AGREGAMOS ESTA FUNCION PARA CONTROLAR QUE DEPENDIENDO DEL TIPO DE POLITICA IMPRIMA UNA COSA O LA OTRA
+        let controlarPolitica = a => (tiempoDeEntrega > longitud) ? 
+        <h4>Pedir {inventario} unidades cuando el inventario baje de {puntoDeReorden} unidades</h4> : <h4>Pedir {inventario} unidades cada {longitud} unidades de tiempo</h4>; 
+        
+        
         
         return (
             <Container fluid className="App">
@@ -155,6 +164,9 @@ class modelStockSimple extends React.Component{
                         <h6>El tiempo de entrega es: ${this.state.tiempoDeEntrega}</h6>
                         <h4>Cantidad economica de pedido y*= {inventario}</h4>
                         <h4>Longitud del ciclo t0*= {longitud}</h4>
+                        <h4>El costo de inventario TCU(y) es: {TCU}</h4>
+                        <h4>El punto de reorden es: {puntoDeReorden}</h4>
+                        {controlarPolitica()}
                     </Col>
 
                     <Row className="btn-volver justify-content-center">
