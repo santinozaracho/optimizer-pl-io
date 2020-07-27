6a32f6da110bf6e626104d4529c189fd06b3efe2
Exequiel Schuster <exe97as@gmail.com>
2020-07-27 18:07:21 -0300

    Estaria arreglado la funcion gradiente

--- src/components/NoLinealProgramming/Methods/Gradiente.js
+++ src/components/NoLinealProgramming/Methods/Gradiente.js
@@ -1,73 +1,119 @@
-const { create, all } = require("mathjs");
+const math = require("mathjs");
 
-const math = create(all);
+// Funcion que engloba todas las funciones
+function funcionGradiente (f,x0,e) {
 
-const separaVariables = (x) => {
-  const scope = x.map((xi, i) => {
-    const name = `x${i + 1}`;
-    return { [name]: xi };
-  });
+  const separaVariables = (x) => {
+    const scope = x.map((xi, i) => {
+      const name = `x${i + 1}`;
+      return { [name]: xi };
+    });
+    
+    const target = {};
+    for (let i = 0; i < scope.length; i += 1) {
+      Object.assign(target, scope[i]);
+    }
+    return target;
+  };
 
-  const target = {};
-  for (let i = 0; i < scope.length; i += 1) {
-    Object.assign(target, scope[i]);
-  }
-  return target;
+  // Calcula la funcion para un valor de x
+  const calculaFuncion = (f, x) => {
+    return f.evaluate(separaVariables(x));
+  };
+
+  // deriva la funcion respecto de xi
+  const derivadaParcial = (f, i) => {
+    return math.derivative(f, `x${i}`);
+  };
+
+  // Indica la direccion en que se tiene que seguir para la proxima iteracion
+  const calculaGradiente = (f, x) => x.map((xi, i) => derivadaParcial(f, i + 1));
+
+  // Calcula el gradiente 
+  const valorGradiente = (x, gradiente) => {
+    const g = [];
+    for (let i = 0; i < x.length; i += 1) {
+      g.push(calculaFuncion(gradiente[i], x));
+    }
+    return g;
+  };
+
+  // Verifica que se cumpla la condicion de parada
+  const criterioParada = (x, gradiente) => {
+    const vlrG = valorGradiente(x, gradiente);
+    return math.norm(vlrG);
+  };
+
+  const llamadoGradiente = ( f, x0, e ) => {
+    // Define vetor gradiente
+    if (e == 0){
+      e = 0.1;
+    }
+
+    const gradiente = calculaGradiente(f, x0);
+
+    // Here is where the sht happen
+    return new Promise((resolve) => {
+      const minimize = (f, e) => {
+        let eps = e;
+        if (e === undefined) eps = 1;
+      
+        const d1 = math.derivative(f, "x");
+        const d2 = math.derivative(d1, "x");
+      
+        // Calcula el punto proximo
+        const min = (k, x) => {
+          if (k > 99) return x;
+      
+          const f1 = d1.evaluate({ x });
+          const f2 = d2.evaluate({ x });
+      
+          if (math.abs(f1) < eps * 0.001) return x;
+      
+          // calcula el proximo Xi
+          const xn = x - f1 / f2;
+          
+          // Indica si se cumple con la condicion de parada
+          if (math.abs(xn - x) / math.max(math.abs(xn), 1) < eps * 0.001) return xn;
+      
+          return min(k + 1, xn);
+        };
+      
+        return min(0, 0);
+      };
+
+      const min = (k, x) => {
+        let fP = f;
+
+        // Define la direccion de busqueda
+        const d = math.multiply(-1, valorGradiente(x, gradiente));
+
+        // Substituye incognitas por xk+lambda*d
+        for (let i = 0; i < x0.length; i += 1)
+          fP = fP.replace(new RegExp(`x${i + 1}`, "g"), `(${x[i]}+x*${d[i]})`);
+
+        // Encuentra el valor de lambda
+        const lambda = minimize(fP);
+
+        // Encuentra el nuevo valor de x
+        // eslint-disable-next-line no-param-reassign
+        x = math.add(x, math.multiply(lambda, d));
+
+        if (k > 9999) return x; // Numero de interaciones alto
+        if (criterioParada(x, gradiente) < e) return x; // Para CP < e
+        return min(k + 1, x);
+      };
+      return resolve(
+        `X* = (${min(0, x0)
+          .map((xi) => xi.toFixed(4))
+          .join(" ")})`
+      );
+    });
+  };
+
+  return llamadoGradiente(f,x0,e);
 };
 
-const calculaFuncion = (f, x) => {
-  return f.evaluate(separaVariables(x));
-};
+module.exports = {funcionGradiente};
 
-const derivadaParcial = (f, i) => {
-  return math.derivative(f, `x${i}`);
-};
-
-const calculaGradiente = (f, x) => x.map((xi, i) => derivadaParcial(f, i + 1));
-
-const valorGradiente = (x, gradiente) => {
-  const g = [];
-  for (let i = 0; i < x.length; i += 1) {
-    g.push(calculaFuncion(gradiente[i], x));
-  }
-  return g;
-};
-
-const criterioParada = (x, gradiente) => {
-  const vlrG = valorGradiente(x, gradiente);
-  return math.norm(vlrG);
-};
-
-module.exports = function funcionGradiente({ f, x0, e, minimize }) {
-  // Define vetor gradiente
-  const gradiente = calculaGradiente(f, x0);
-
-  return new Promise((resolve) => {
-    const min = (k, x) => {
-      let fP = f;
-
-      // Define la direccion de busqueda
-      const d = math.multiply(-1, valorGradiente(x, gradiente));
-
-      // Substituye incognitas por xk+lambda*d
-      for (let i = 0; i < x0.length; i += 1)
-        fP = fP.replace(new RegExp(`x${i + 1}`, "g"), `(${x[i]}+x*${d[i]})`);
-
-      // Encuentra el valor de lambda
-      const lambda = minimize(fP);
-
-      // Encuentra el nuevo valor de x
-      // eslint-disable-next-line no-param-reassign
-      x = math.add(x, math.multiply(lambda, d));
-
-      if (k > 9999) return x; // Numero de interaciones alto
-      if (criterioParada(x, gradiente) < e) return x; // Para CP < e
-      return min(k + 1, x);
-    };
-    return resolve(
-      `X* = (${min(0, x0)
-        .map((xi) => xi.toFixed(4))
-        .join(" ")})`
-    );
-  });
-};
+//Algo asi seria el llamado = funcionGradiente('(-x1)^2 - (x2+1)^2',[0,0],0.1).then((p) => { console.log(p.toString()) });
