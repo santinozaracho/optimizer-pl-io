99700b6a19df3c7808a6cdc692c055522199906b
AnastaciusWright <alexandronadal@gmail.com>
2020-07-28 18:41:38 -0300

    Devuelve promise

--- src/components/NoLinealProgramming/Methods/lagrangeMul.js
+++ src/components/NoLinealProgramming/Methods/lagrangeMul.js
@@ -7,7 +7,7 @@ const fetch = require('node-fetch');
 
 
 //f is the function, g is an array of constraints
-const lagrangeMul =(f,g, objective) => {
+const lagrangeMul = async (f,g, objective) => {
     var nConstraints= g.length
 
 
@@ -19,7 +19,7 @@ const lagrangeMul =(f,g, objective) => {
     var ladoIzqRestriccion = [];
     var lagrangeExpr;
     var variables;
-    var n= 0; //number of variables
+    var n = 0; //number of variables
     var todosLagrange=[];
     g.forEach(element => {
         ladoIzqRestriccion.push(element.split('=')[0])
@@ -33,6 +33,7 @@ const lagrangeMul =(f,g, objective) => {
         i+=1
     });
 
+    // Create te URL
     var url = 'https://nlsystemsolver.herokuapp.com/getmsg/' 
     var variablesURL='';
     var ecuacionesURL='';
@@ -47,18 +48,13 @@ const lagrangeMul =(f,g, objective) => {
     ecuacionesURL = ecuacionesURL.slice(0,ecuacionesURL.length-1);
     variablesURL = variablesURL.slice(0,variablesURL.length-1);
 
+    // Modify the URL to the Python program
     url = url + '?ecuaciones=' + ecuacionesURL + '&variables=' + variablesURL;
+    url = url.split('+').join('%2B'); // Replace every plus with a %2B
+    url = url.split(/\s/g).join(''); // Eliminate spaces for url
+    url = url.split('^').join('**'); // Change ^ to **
     
-    // Replace every plus with a %2B
-    url = url.split('+').join('%2B');
-
-    // Eliminate spaces for url
-    url = url.split(/\s/g).join('');
-
-    // Change ^ to **
-    url = url.split('^').join('**');
-    
-    x0=[]
+    var x0=[]
     /*// Fetch to get the variables values
     var respuesta;
     const traerValores = async (url,callback) => {
@@ -71,160 +67,45 @@ const lagrangeMul =(f,g, objective) => {
         })
         return x1;
     }*/
-
-    // peto 
-    // Fetch to get the variables values
-    const traerValores = async (url,callback) => {
-        const variable = await fetch(url,{method:'GET'})
-        .then(res => res.json())
-        .then(json => {
-            x1=[]
-            return callback(json.MESSAGE)
-            // return await x1;
+    
+    var traerValores = async (url) => {
+        try{
+            var resultadoFetch = await fetch(url,{method:'GET'})
+                .then(res => res.json())
+                .then(async respuesta => {
+            var respuestaStr = respuesta.MESSAGE
+            //respuestaStr = JSON.stringify(respuestaStr);
+            //console.log(typeof respuestaStr)
+            respuestaStr = respuestaStr.replace("[","");
+            respuestaStr = respuestaStr.replace("]","");
+            // Paso a un arreglo
+            respuestaStr = respuestaStr.replace(/'/g,"");
+            respuestaStr = respuestaStr.replace(/ /g,"");
+            respuestaStr = respuestaStr.split(",");
+            return respuestaStr
         });
+        return resultadoFetch
+        }
+        catch(e)
+        {
+            console.log(e)
+        }
         
-
     }
 
-    callbackFunction = (data) =>{
-        // console.log(data)
-        respuesta = data
-        var stringRespuesta =''
-        var num;
-        var dem;
-        var estadoFraccion = false // Controls if we are processing a fraction or not
-        var cont = 0
-        respuesta = respuesta.replace("[","");
-        respuesta = respuesta.replace("]","");
-        // Paso a un arreglo
-        respuesta = respuesta.replace(/'/g,"")
-        respuesta = respuesta.replace(/ /g,"")
-        respuesta = respuesta.split(",");
-        //console.log('Respuesta')
-        ///console.log(respuesta)
-        //console.log('Tipo de respuesta: '+typeof respuesta)
-        var x0=[]
-        respuesta.forEach(element => {
-            // Do some refactoring here in some distant future
-            x0.push(eval((element.split('sqrt').join('Math.sqrt')).toString()))
-        })
-        //console.log('x0')
-        //console.log(x0)
-
-        var m = ladoIzqRestriccion.length; //Columns number of restrictions
-        n = n-g.length //substracting the lambdas
-        
-        // Generate P array, where [[Ng1(x), Ng1(x2)],[Ng2(x1),Ng2(x2)]]
-        var P = math.zeros(m,n)
-        
-        var tempRestriccion;
-        var varP;
-        var scope = {}
-        for (let k = 0; k < variables.length; k++) {
-            scope[variables[k]]=x0[k]    
-        }
-        // console.log('scope')
-        // console.log(scope)
-        for(var i = 0; i < m; i+=1) //each restriction
-        {
-            for(var j = 0; j < n; j+=1) //each variable
-            {
-                tempRestriccion = ladoIzqRestriccion[i];   
-                varP = variables[j].toString();
-                tempRestriccion = math.derivative(tempRestriccion, varP); 
-                tempRestriccionParsed = Parser.parse(tempRestriccion.toString());
-                P.subset(math.index(i, j), tempRestriccionParsed.evaluate({ varP : x0[j] }));
-            }
-        }
-
-        // Load P in Hessiano
-        var hessiano = math.zeros(m+n,m+n)
-        for(var i = 0; i < m; i+=1) //each restriction
-        {
-            for(var j = n-1; j < m+n; j+=1) //each variable
-            {
-                hessiano.subset(math.index(i, j), P.subset(math.index(i,j-n+1)));
-            }
-        }
-
-        // Load P^T in Hessiano
-        for(var i = n-1; i < m+n; i+=1) //each restriction
-        {
-            for(var j = 0; j < m; j+=1) //each variable
-            {   
-                hessiano.subset(math.index(i, j), P.subset(math.index(j,i-n+1)));
-            }
-        }
-        
-        //Generate Q
-        var derivadasPrimeras=[]
-        var derivadasSegundas=[]
-        var derivSegEvaluadas =[]
-        var tempDerSeg;
-        // console.log('n')
-        //console.log(n)
-
-        var Q= math.zeros(n,n)
-        //console.log("Q")
-        // console.log(Q)
-        for(let k = 0; k < n; k++)
-        {
-            derivadasPrimeras.push(math.derivative(lagrange,variables[k]).toString())
-            for(let l = 0; l < n; l++)
-            {
-                tempDerSeg = math.derivative(derivadasPrimeras[k],variables[l]).toString()
-                derivadasSegundas.push(tempDerSeg)
-                tempDerSeg = math.evaluate(tempDerSeg,scope)
-                derivSegEvaluadas.push(tempDerSeg)
-                Q.subset(math.index(k,l),tempDerSeg)
-            }
-        }
-        // console.log("Q")
-        // console.log(Q)
-
-        //copy Q into Hessian Matrix
-        for(var i = 0;i < n; i+=1){
-            for (let j = 0; j < n; j++) {
-                hessiano.subset(math.index(m+i,m+j), Q.subset(math.index(i,j)))
-            }
-        }
-        
-        // -----------------------------------------
-        var matrizHessianaMath = math.clone(math.matrix(hessiano))
-        var subMatrices=[]
-        // [subMatrizn,subMatrizn-1,subMatrizn-2]
-        var SubDeterminantes = []
-        // [Detn,Detn-1,Detn-2]
-        for (let i = m+n; i >= (m+n)-(n-m); i--) {
-            //meto al arreglo de subDeterminantes de atras para adelante,porque el reize me trimea los datos
-            subMatrices.push(math.clone(matrizHessianaMath.resize([i,i])))
-        }
-        //ahora que tengo las submatrices, calculo los subdeterminantes
-        subMatrices.forEach(subMat=>{
-            SubDeterminantes.push(math.det(subMat._data))
-        })
-        //console.log("Determinantes")
-        //console.log(SubDeterminantes)
-
-        //------------------------------------------
-        //console.log(derivadasPrimeras.toString())
-        //console.log(derivadasSegundas.toString())
-        
-        //console.log(hessiano.toString())
-        //console.log(x0)
-        return x0    
-    }
-    
-    // x0=traerValores(url,callbackFunction);
-
-    traerValores(url,callbackFunction).then(result => console.log(result))
-    
-    
-    //return x0.splice(0,n);
+    return traerValores(url);
 }
 
 // URL Should be like --> https://nlsystemsolver.herokuapp.com/getmsg/?ecuaciones=1-2*x;z-2*y;2%2By-2*z&variables=x,y,z
 // 2*x**2-L1*3x  "2*x^2",["3*x=12"],"max"
 //console.log(lagrangeMul("-x1^2 -(x2 -1)^2",["2*x1+x2-1=0"],"min"));
 //console.log(lagrangeMul("x^2+y^2+z^2",["x+y+3*z-2=0","5*x+2*y+z-5=0"],"max"));
-console.log(lagrangeMul("-(x1)^2-(x2)^2-(x3)^2+4*x1+8*x2+16*x3",["x1+x2+x3-2=0","x1+2*x2=0"],"max"));
+/*var odioMiVida = lagrangeMul("-(x1)^2-(x2)^2-(x3)^2+4*x1+8*x2+16*x3",["x1+x2+x3-2=0","x1+2*x2=0"],"max")
+    .then()
+    .then(value => {
+        return value
+    });
+
+console.log(odioMiVida)*/
+
+module.exports ={lagrangeMul};
