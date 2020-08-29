9215bcaaa3207ddebde9a15b89d739fdf3c9b582
Nicolas <nikoalegre@hotmail.com>
2020-08-29 13:54:57 -0300

    Agregué el collapse a las vistas de info de todos los modelos

--- src/components/StockProblems/CantidadEconomicaPedido/info.js
+++ src/components/StockProblems/CantidadEconomicaPedido/info.js
@@ -1,12 +1,34 @@
-import React from "react";
-import {Button, Container, Row, Col, Card, CardText, CardTitle, Jumbotron} from "reactstrap";
+import React,{useState} from "react";
+import {Button, Container, Row, Col, Card, CardText, CardTitle, Jumbotron, Collapse} from "reactstrap";
 import {Link} from 'react-router-dom';
 import '../index.css'
 import MathJax from "react-mathjax"
 
 
 
-const info = () => {
+const Info = () => {
+    //DEFINIMOS ESTOS ESTADOS PARA MANEJAR LOS COLLAPSE
+    //Para controlar el collapse de Hipotesis
+    const [collapseHipotesis, setCollapseHipotesis] = useState(false);
+    const [statusHipotesis, setStatusHipotesis] = useState('+');
+    const onEnteredHipotesis = () => setStatusHipotesis('-');
+    const onExitedHipotesis = () => setStatusHipotesis('+');
+    const toggleHipotesis = () => setCollapseHipotesis(!collapseHipotesis);
+
+    //Para controlar el collapse de Formulas
+    const [collapseFormulas, setCollapseFormulas] = useState(false);
+    const [statusFormulas, setStatusFormulas] = useState('+');
+    const onEnteredFormulas = () => setStatusFormulas('-');
+    const onExitedFormulas = () => setStatusFormulas('+');
+    const toggleFormulas = () => setCollapseFormulas(!collapseFormulas);
+
+    //Para controlar el collapse de Variables
+    const [collapseVariables, setCollapseVariables] = useState(false);
+    const [statusVariables, setStatusVariables] = useState('+');
+    const onEnteredVariables = () => setStatusVariables('-');
+    const onExitedVariables = () => setStatusVariables('+');
+    const toggleVariables = () => setCollapseVariables(!collapseVariables);
+
     return(
     <Container fluid className="App">
       <Row>
@@ -20,68 +42,75 @@ const info = () => {
             
             <Row style={{marginBottom:10}}>
                 <Card body outline color="secondary" >
-                    <Row className="justify-content-center">
-                        <h5><b>Hipotesis</b></h5>
+                    <Row className="justify-content-center" onClick={toggleHipotesis} style={{cursor:"pointer"}}>
+                        <h5><b>Hipotesis {statusHipotesis}</b></h5>
                     </Row>
-                    <ul className='lista'>
-                        <li>No tenemos en cuenta si contamos o no con los recursos financieros</li>
-                        <li>No hay inflasión</li>
-                        <li>No se permiten faltantes</li>
-                        <li>Demanda constante y conocida.</li>
-                        <li>Resposición instantánea.</li>
-                        <li>Costo unitario de almacenamiento por unidad de tiempo h, constante.</li>
-                        <li>Costo de preparacion 𝑘, constante.</li>
-                        <li>No existen otros costos.</li>
-                        <li>Al comienzo de cada periodo no hay stock ni pedidos insatisfechos.</li>
-                    </ul>
+                    <Collapse isOpen={collapseHipotesis} onEntered={onEnteredHipotesis} onExited={onExitedHipotesis}>
+                        <ul className='lista'>
+                            <li>No tenemos en cuenta si contamos o no con los recursos financieros</li>
+                            <li>No hay inflasión</li>
+                            <li>No se permiten faltantes</li>
+                            <li>Demanda constante y conocida.</li>
+                            <li>Resposición instantánea.</li>
+                            <li>Costo unitario de almacenamiento por unidad de tiempo h, constante.</li>
+                            <li>Costo de preparacion 𝑘, constante.</li>
+                            <li>No existen otros costos.</li>
+                            <li>Al comienzo de cada periodo no hay stock ni pedidos insatisfechos.</li>
+                        </ul>
+                    </Collapse>
                 </Card>
             </Row>
             <Row style={{marginBottom:10}}>
                 <Card body outline color="secondary">
-                <Row className="justify-content-center">
-                        <h5><b>Formulas</b></h5>
+                <Row className="justify-content-center" onClick={toggleFormulas} style={{cursor:"pointer"}}>
+                    <h5><b>Formulas {statusFormulas}</b></h5>
                 </Row>
-                        
-                <MathJax.Provider>
-                    <div>
-                        <MathJax.Node formula={"Tiempo De Ciclo = \\frac{y}{D}"} />
-                    </div>
-                </MathJax.Provider>
-                <MathJax.Provider>
-                    <div>
-                        <MathJax.Node formula={"Costo De Preparacion = \\frac{K}{\\frac{y}{D}}"} />
-                    </div>
-                </MathJax.Provider>
-                <MathJax.Provider>
-                    <div>
-                        <MathJax.Node formula={"Costo Total De Almacenamiento = h*\\frac{y}{2}"} />
-                    </div>
-                </MathJax.Provider>
-                <MathJax.Provider>
-                    <div>
-                        <MathJax.Node formula={"Costo Total Por Unidad De Tiempo = \\frac{K}{\\frac{y}{D}}+h*\\frac{y}{2}"} />
-                    </div>
-                </MathJax.Provider>
-                <MathJax.Provider>
-                    <div>
-                        <MathJax.Node formula={"y* = \\sqrt{\\frac{2*K*D}{h}}"} />
-                    </div>
-                </MathJax.Provider>    
+
+                <Collapse isOpen={collapseFormulas} onEntered={onEnteredFormulas} onExited={onExitedFormulas}>   
+                    <MathJax.Provider>
+                        <div>
+                            <MathJax.Node formula={"Tiempo De Ciclo = \\frac{y}{D}"} />
+                        </div>
+                    </MathJax.Provider>
+                    <MathJax.Provider>
+                        <div>
+                            <MathJax.Node formula={"Costo De Preparacion = \\frac{K}{\\frac{y}{D}}"} />
+                        </div>
+                    </MathJax.Provider>
+                    <MathJax.Provider>
+                        <div>
+                            <MathJax.Node formula={"Costo Total De Almacenamiento = h*\\frac{y}{2}"} />
+                        </div>
+                    </MathJax.Provider>
+                    <MathJax.Provider>
+                        <div>
+                            <MathJax.Node formula={"Costo Total Por Unidad De Tiempo = \\frac{K}{\\frac{y}{D}}+h*\\frac{y}{2}"} />
+                        </div>
+                    </MathJax.Provider>
+                    <MathJax.Provider>
+                        <div>
+                            <MathJax.Node formula={"y* = \\sqrt{\\frac{2*K*D}{h}}"} />
+                        </div>
+                    </MathJax.Provider> 
+                </Collapse>   
                 </Card>
             </Row>
             <Row style={{marginBottom:10}}>
                 <Card body outline color="secondary">
-                    <Row className="justify-content-center">
-                        <h5><b>Variables</b></h5>
+                    <Row className="justify-content-center" onClick={toggleVariables} style={{cursor:"pointer"}}>
+                        <h5><b>Variables {statusVariables}</b></h5>
                     </Row>
-                    <ul className='lista'>
-                        <li><b>D: </b>Demanda</li>
-                        <li><b>K: </b>Costo de preparacion</li>
-                        <li><b>h: </b>Costo de almacenamiento</li>
-                        <li><b>L: </b>Tiempo de entrega</li>
-                        <li><b>t0*: </b>Longitud de ciclo</li>
-                        <li><b>y*: </b>Cantidad economica</li>  
-                    </ul>
+
+                    <Collapse isOpen={collapseVariables} onEntered={onEnteredVariables} onExited={onExitedVariables}>
+                        <ul className='lista'>
+                            <li><b>D: </b>Demanda</li>
+                            <li><b>K: </b>Costo de preparacion</li>
+                            <li><b>h: </b>Costo de almacenamiento</li>
+                            <li><b>L: </b>Tiempo de entrega</li>
+                            <li><b>t0*: </b>Longitud de ciclo</li>
+                            <li><b>y*: </b>Cantidad economica</li>  
+                        </ul>
+                    </Collapse>
                 </Card>
             </Row>
             <Row className="justify-content-left">
@@ -101,4 +130,4 @@ const info = () => {
     </Container>
     )
     };
-export default info;
+export default Info;
--- src/components/StockProblems/ModeloSimpleSinAgotamiento/info.js
+++ src/components/StockProblems/ModeloSimpleSinAgotamiento/info.js
@@ -1,12 +1,33 @@
-import React from "react";
-import {Button, Container, Row, Col, Card, CardText, Jumbotron, CardTitle} from "reactstrap";
+import React, {useState} from "react";
+import {Button, Container, Row, Col, Card, CardText, Jumbotron, CardTitle, Collapse} from "reactstrap";
 import {Link} from 'react-router-dom';
 import '../index.css'
 import MathJax from "react-mathjax"
 
 
 
-const infoModeloSimpleSinAgotamiento = () => {
+const InfoModeloSimpleSinAgotamiento = () => {
+    //DEFINIMOS ESTOS ESTADOS PARA MANEJAR LOS COLLAPSE
+    //Para controlar el collapse de Hipotesis
+    const [collapseHipotesis, setCollapseHipotesis] = useState(false);
+    const [statusHipotesis, setStatusHipotesis] = useState('+');
+    const onEnteredHipotesis = () => setStatusHipotesis('-');
+    const onExitedHipotesis = () => setStatusHipotesis('+');
+    const toggleHipotesis = () => setCollapseHipotesis(!collapseHipotesis);
+
+    //Para controlar el collapse de Formulas
+    const [collapseFormulas, setCollapseFormulas] = useState(false);
+    const [statusFormulas, setStatusFormulas] = useState('+');
+    const onEnteredFormulas = () => setStatusFormulas('-');
+    const onExitedFormulas = () => setStatusFormulas('+');
+    const toggleFormulas = () => setCollapseFormulas(!collapseFormulas);
+
+    //Para controlar el collapse de Variables
+    const [collapseVariables, setCollapseVariables] = useState(false);
+    const [statusVariables, setStatusVariables] = useState('+');
+    const onEnteredVariables = () => setStatusVariables('-');
+    const onExitedVariables = () => setStatusVariables('+');
+    const toggleVariables = () => setCollapseVariables(!collapseVariables);
     return(
     <Container fluid className="App">
       <Row>
@@ -20,69 +41,76 @@ const infoModeloSimpleSinAgotamiento = () => {
             
             <Row style={{marginBottom:10}}>
                 <Card body outline color="secondary" >
-                    <Row className="justify-content-center">
-                        <h5><b>Hipotesis</b></h5>
+                    <Row className="justify-content-center" onClick={toggleHipotesis} style={{cursor:"pointer"}}>
+                        <h5><b>Hipotesis {statusHipotesis}</b></h5>
                     </Row>
-                    <ul className='lista'>
-                        <li>Tasa constante de demanda con el surtido instantáneo del pedido y sin faltante</li>
-                        <li>Una vez pedido el stock se actualiza automaticamente.</li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
+                    <Collapse isOpen={collapseHipotesis} onEntered={onEnteredHipotesis} onExited={onExitedHipotesis}>
+                        <ul className='lista'>
+                            <li>Tasa constante de demanda con el surtido instantáneo del pedido y sin faltante</li>
+                            <li>Una vez pedido el stock se actualiza automaticamente.</li>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </Collapse>
                 </Card>
             </Row>
             <Row style={{marginBottom:10}}>
                 <Card body outline color="secondary">
-                <Row className="justify-content-center">
-                        <h5><b>Formulas</b></h5>
+                <Row className="justify-content-center" onClick={toggleFormulas} style={{cursor:"pointer"}}>
+                    <h5><b>Formulas {statusFormulas}</b></h5>
                 </Row>
-                        
-                <MathJax.Provider>
-                    <div>
-                        <MathJax.Node formula={"Costo Total De Preparacion = \\frac{D}{q}*K"} />
-                    </div>
-                </MathJax.Provider>
-                <MathJax.Provider>
-                    <div>
-                        <MathJax.Node formula={"Costo Total Del Producto = bi*D"} />
-                    </div>
-                </MathJax.Provider>
-                <MathJax.Provider>
-                    <div>
-                        <MathJax.Node formula={"Costo Total De Almacenamiento = \\frac{1}{2}*q*T*Ci"} />
-                    </div>
-                </MathJax.Provider>
-                <MathJax.Provider>
-                    <div>
-                        <MathJax.Node formula={"Costo Del Dinero Inmovilizado = P*bi"} />
-                    </div>
-                </MathJax.Provider>
-                <MathJax.Provider>
-                    <div>
-                        <MathJax.Node formula={"Costo Total Esperado = \\frac{D}{q}*K + b*K + \\frac{1}{2}*q*T*Ci + P*bi"} />
-                    </div>
-                </MathJax.Provider>    
-                <MathJax.Provider>
-                    <div>
-                        <MathJax.Node formula={"q_{0i} = \\sqrt{\\frac{2*K*D}{T*(P*bi*Ci)}}   "} />
-                    </div>
-                </MathJax.Provider>
+
+                <Collapse isOpen={collapseFormulas} onEntered={onEnteredFormulas} onExited={onExitedFormulas}>        
+                    <MathJax.Provider>
+                        <div>
+                            <MathJax.Node formula={"Costo Total De Preparacion = \\frac{D}{q}*K"} />
+                        </div>
+                    </MathJax.Provider>
+                    <MathJax.Provider>
+                        <div>
+                            <MathJax.Node formula={"Costo Total Del Producto = bi*D"} />
+                        </div>
+                    </MathJax.Provider>
+                    <MathJax.Provider>
+                        <div>
+                            <MathJax.Node formula={"Costo Total De Almacenamiento = \\frac{1}{2}*q*T*Ci"} />
+                        </div>
+                    </MathJax.Provider>
+                    <MathJax.Provider>
+                        <div>
+                            <MathJax.Node formula={"Costo Del Dinero Inmovilizado = P*bi"} />
+                        </div>
+                    </MathJax.Provider>
+                    <MathJax.Provider>
+                        <div>
+                            <MathJax.Node formula={"Costo Total Esperado = \\frac{D}{q}*K + b*K + \\frac{1}{2}*q*T*Ci + P*bi"} />
+                        </div>
+                    </MathJax.Provider>    
+                    <MathJax.Provider>
+                        <div>
+                            <MathJax.Node formula={"q_{0i} = \\sqrt{\\frac{2*K*D}{T*(P*bi*Ci)}}   "} />
+                        </div>
+                    </MathJax.Provider>
+                </Collapse>
                 </Card>
             </Row>
             <Row style={{marginBottom:10}}>
                 <Card body outline color="secondary">
-                    <Row className="justify-content-center">
-                        <h5><b>Variables</b></h5>
+                <Row className="justify-content-center" onClick={toggleVariables} style={{cursor:"pointer"}}>
+                        <h5><b>Variables {statusVariables}</b></h5>
                     </Row>
-                    <ul className='lista'>
-                        <li><b>D: </b>Demanda</li>
-                        <li><b>K: </b>Costo de preparacion</li>
-                        <li><b>c1: </b>Costo de almacenamiento</li>
-                        <li><b>P: porcentaje de interés que se produciría con el dinero inmovilizado</b></li>
-                        <li><b>C’i: Costo efectivo de almacenamient</b></li>
-                        <li><b>bi: Costo  del i-esimoproducto</b></li>
-                    </ul>
+
+                    <Collapse isOpen={collapseVariables} onEntered={onEnteredVariables} onExited={onExitedVariables}>
+                        <ul className='lista'>
+                            <li><b>D: </b>Demanda</li>
+                            <li><b>K: </b>Costo de preparacion</li>
+                            <li><b>c1: </b>Costo de almacenamiento</li>
+                            <li><b>P: porcentaje de interés que se produciría con el dinero inmovilizado</b></li>
+                            <li><b>C’i: Costo efectivo de almacenamient</b></li>
+                            <li><b>bi: Costo  del i-esimoproducto</b></li>
+                        </ul>
+                    </Collapse>
                 </Card>
             </Row>
             <Row className="justify-content-left">
@@ -102,4 +130,4 @@ const infoModeloSimpleSinAgotamiento = () => {
     </Container>
     )
     };
-export default infoModeloSimpleSinAgotamiento;
+export default InfoModeloSimpleSinAgotamiento;
